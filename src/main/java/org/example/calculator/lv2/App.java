7f37a2cceb2b7769f92626a1c61f03934c0c2f77
unknown <twodc@naver.com>
2025-07-11 21:13:24 +0900

    feat: update lv2 calculator

--- src/main/java/org/example/calculator/lv2/App.java
+++ src/main/java/org/example/calculator/lv2/App.java
@@ -1,5 +1,7 @@
 package org.example.calculator.lv2;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class App {
@@ -10,28 +12,42 @@ public class App {
 
         String answer = ""; // do-while 문 안에 선언 시 while에서 변수 인식 못함
         do {
-            System.out.print("첫 번째 숫자를 입력하세요: "); // 입력 값이 정수가 아닐 경우 경고 메시지 출력되게
-            int num1 = scanner.nextInt();
-            System.out.print("두 번째 숫자를 입력하세요: ");
-            int num2 = scanner.nextInt();
+
+            // 첫 번째 숫자
+            int num1 = calculator.isValid(scanner, "첫 번째");
+
+            // 두 번째 숫자
+            int num2 = calculator.isValid(scanner, "두 번째");
+
+            // 사칙연산
             System.out.print("사칙연산 기호를 입력하세요: ");
             char operator = scanner.next().charAt(0);
 
+            // 계산 및 기록
             int result = calculator.calculate(num1, num2, operator);
             System.out.println("결과: " + result);
-            System.out.println("결과 목록: " + calculator.getResults());
+            System.out.println("결과 목록: " + calculator.getResults()); // 게터 활용
 
-            System.out.print("첫 결과 값을 삭제하시겠습니까? 삭제하려면 Y를 입력하세요.: "); // 원하는 값을 삭제할 수 있게
+            // 기록된 값 삭제 기능
+            System.out.print("결과 목록의 첫 번째 값을 삭제하시겠습니까? (Y/N): "); // lv3에선 원하는 값을 삭제할 수 있게
             String delNumber = scanner.next();
             if (delNumber.equalsIgnoreCase("Y")) {
                 calculator.removeResult();
+                System.out.println("결과 목록: " + calculator.getResults()); // 삭제 후 결과 출력
             }
-            System.out.println("결과 목록: " + calculator.getResults());
 
+            // 결과 목록 초기화 기능
+            System.out.print("결과 목록을 초기화 하시겠습니까? (Y/N): ");
+            String reset = scanner.next();
+            if (reset.equalsIgnoreCase("Y")) {
+                List<Integer> newResults = new ArrayList<>();
+                calculator.setResults(newResults); // 세터 활용
+                System.out.println("목록이 초기화 되었습니다.");
+            }
+
+            // 추가 계산 유무
             System.out.println("더 계산하려면 아무거나 입력하세요. exit 입력 시 종료됩니다.");
             answer = scanner.next();
-
         } while (!answer.equalsIgnoreCase("exit")); // 대소문자 무시
-
     }
 }
